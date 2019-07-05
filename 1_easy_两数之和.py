9ffc836f6933b1a636f015185467956e0ad35c3a
“peterpwb <3046222415@qq.com>
2019-07-05 12:37:31 +0800

    提交文件

---
+++ 1_easy_两数之和.py
@@ -0,0 +1,12 @@
+#44 ms  enumerate()函数（索引，值）  
+#Time:O(n)   Space:O(n)  (遍历)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) == 0:
+            return []
+        dic_index = {}
+        for key,val in enumerate(nums):
+            if target-val in dic_index:
+                return [dic_index[target-val],key]
+            else:
+                dic_index[val]=key
