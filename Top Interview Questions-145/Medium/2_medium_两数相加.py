9b8a145c93de40119ac9577aa9a0e12fce6202b8
peterpwb <45564240+peterpwb@users.noreply.github.com>
2019-10-24 22:06:56 +0800

    Add files via upload

---
+++ Top Interview Questions-145/Medium/2_medium_两数相加.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+#100 ms，一次遍历
+#Time:O(max(m,n))   Space:O(max(m,n))
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        res = dim = ListNode(0)#头一位好处理
+        addone = 0
+        while l1 and l2:
+            dim.next = ListNode((l1.val + l2.val +addone)%10)#这样才是加链表
+            addone = (l1.val + l2.val +addone)//10
+            l1 = l1.next
+            l2 = l2.next
+            dim = dim.next
+        #处理最后多处理的进1,可能一直进1
+        l1 = l1 if l1 else l2
+        while addone:
+            if l1:#确定不为空才能加值
+                dim.next = ListNode((l1.val + addone)%10)
+                addone = (l1.val + addone)//10
+                l1 = l1.next
+                dim = dim.next
+            else:#l1为空就剩一个加1
+                dim.next = ListNode(1)
+                dim = dim.next
+                #addone = 0这样写也可以
+                break#始终是1，跳出少一次循环节约时间
+        dim.next = l1
+        return res.next
+        
---
+++ Top Interview Questions-145/Medium/3_medium_无重复字符的最长子串.py
@@ -0,0 +1,18 @@
+#滑动窗口,76ms
+# Time : O(n)  Space: O(1)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        start = 0
+        end = 0
+        maxlen = 0
+        length = len(s)
+        res = set()#用来存无重复字母
+        while end < length:
+            if s[end] not in res:#没有该字母
+                res.add(s[end])
+                end += 1
+                maxlen = max(maxlen,end-start)#取两者中长的那个
+            else:
+                res.remove(s[start])#如果没删到重复元素会一直删
+                start += 1
+        return maxlen
