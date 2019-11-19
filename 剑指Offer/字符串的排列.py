ab5d18ff5dc87d298cb347affd9d633358cb6b0e
peterpwb <45564240+peterpwb@users.noreply.github.com>
2019-11-19 16:39:20 +0800

    Update 字符串的排列.py

--- 剑指Offer/字符串的排列.py
+++ 剑指Offer/字符串的排列.py
@@ -7,4 +7,14 @@ class Solution:
         if not ss:
             return []
         return sorted(list(set(map(''.join, itertools.permutations(ss)))))
-        
+ 
+#运行时间：32ms   占用内存：5628k
+class Solution:
+    def Permutation(self, ss):
+        if len(ss) <= 1:
+            return ss
+        res = set()
+        for i in range(len(ss)):
+            for j in self.Permutation(ss[:i] + ss[i+1:]):
+                res.add(ss[i]+j)
+        return sorted(res) # sorted()能对可迭代对象进行排序,结果返回一个新的list
