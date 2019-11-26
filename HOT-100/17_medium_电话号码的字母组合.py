fba73d874ca0d92909eb791a777fbe9b574c41aa
peterpwb <45564240+peterpwb@users.noreply.github.com>
2019-11-26 20:17:56 +0800

    Update 17_medium_电话号码的字母组合.py

--- HOT-100/17_medium_电话号码的字母组合.py
+++ HOT-100/17_medium_电话号码的字母组合.py
@@ -9,3 +9,15 @@ class Solution:
         for i in digits:
             res = [m+n for m in res for n in dic[i]]
         return res
+    
+#迭代器的运用，28ms
+import itertools
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        dic = {'2':"abc",'3':"def",'4':"ghi",'5':"jkl",'6':"mno",'7':"pqrs",'8':"tuv",'9':"wxyz"}
+        res = []
+        if digits is "":
+            return res
+        for i in range(len(digits)):
+            res.append(dic[digits[i]])#["abc","def"]
+        return list(map("".join,itertools.product(*res)))#*的使用
